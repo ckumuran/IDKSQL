9b6fb4e52229c955659fa16e2a0a2795cb1ce03a
kumaran <k31311558@gmail.com>
2026-05-23 12:33:40 +0530

    Update query.ts

--- apps/vscode-extension/src/types/query.ts
+++ apps/vscode-extension/src/types/query.ts
@@ -1,1 +1,15 @@
+export interface QueryRequest {
 
+    databaseType: string;
+
+    userQuery: string;
+}
+
+export interface QueryResponse {
+
+    sql: string;
+
+    result: any[];
+
+    requiresConfirmation?: boolean;
+}
